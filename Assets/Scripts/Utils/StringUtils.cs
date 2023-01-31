8e3d79ee8dd907c17cf07ac0f26ae50c8c43400d
Spektraz <foteskin18@gmail.com>
2023-01-31 21:57:34 +0200

    [FIX] Refactor p2

--- Assets/Scripts/Utils/StringUtils.cs
+++ Assets/Scripts/Utils/StringUtils.cs
@@ -4,7 +4,6 @@ namespace Utils
 {
     public static class StringUtils
     {
-        public const int HOURS_PER_DAY = 24;
 
         public static string TimeSpanToFormattedString(TimeSpan timeSpan)
         {
@@ -15,26 +14,6 @@ namespace Utils
 
             return timeSpan.ToString(@"m\:ss");
         }
-
-        // public static string TimeSpanToFormattedStringMinSec(TimeSpan timeSpan)
-        // {
-        //     return timeSpan.ToString(@"mm\:ss");
-        // }
-        // public static string GetDayTimeSpanFormatted(TimeSpan timeSpan)
-        // {
-        //     if (timeSpan.Days > 0)
-        //     {
-        //         return string.Format($"{timeSpan.Days * HOURS_PER_DAY + timeSpan.Hours}:{timeSpan.Minutes}:{timeSpan.Seconds}");
-        //     }
-        //     else
-        //     {
-        //         return timeSpan.ToString(@"%h\:mm\:ss");
-        //     }
-        // }
-        //
-        // public static string BoolToYesNo(bool value)
-        // {
-        //     return value ? "yes" : "no";
-        // }
+        
     }
 }
